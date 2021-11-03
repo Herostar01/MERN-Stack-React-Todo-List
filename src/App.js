8c81a6cab07edd94e72028044d671a7d6522dc35
Muhammad Shahbaz <M023Shah@gmail.com>
2021-11-03 13:09:57 -0700

    comit_one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  /* Below array destructure syntax is equivalent to:
+  const newTodoStateArr =  useState("");
+  const newTodo = newTodoStateArr[0];
+  const setNewTodo = newTodoStateArr[1];
+    */
+
+  // This one line below is short for above
+
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false 
+    }
+
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+  }
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((x, i) => { 
+      return i !== delIdx; 
+    });
+
+    setTodos(filteredTodos);
+  }
+
+  const handleToggle = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (idx === i) {
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}
+    >
+
+      <form onSubmit={(event) => {
+        handleNewTodoSubmit(event);
+      }} >
+
+        <input onChange={(event) => {
+          setNewTodo(event.target.value);
+        }} type="text"
+          value={newTodo}
+        />
+
+        <div>
+          <button>Add</button>
+        </div>
+
+
+      </form>
+
+      <br />
+
+      {todos.map((todo, i) => {
+        const todoClasses = ['bold', 'italic']; 
+
+        if (todo.complete) {     
+          todoClasses.push("line-through")
+        }
+
+
+        return (
+          <div key={i} >
+            <input onChange={(event) => {
+              handleToggle(i)
+            }} checked={todo.complete} type="checkbox" />
+
+            <span className={todoClasses.join(" ")} > {todo.text} </span> 
+
+            <button onClick={(event) => {
+              handleTodoDelete(i);
+            }}
+
+            >
+              Delete
+            </button>
+
+
+          </div>
+        )
+      })}
+
     </div>
   );
 }
---
+++ src/components/form.jsx
@@ -0,0 +1,36 @@
+// import React from "react";
+// import StyledBox from "./StyledBox";
+
+// const Form = (props) => {
+//     const creatBlock = (e) => {
+//         e.preventDefault(); 
+//         const StyledBox = styled.div`
+//     border: 1px solid lightgray;
+//     background: ${props => props.bgColor};
+//     width: ${props => props.width || '100px'};
+//     height: ${props => props.height || '100px'};
+// `;
+//         console.log() 
+//     };
+
+//     return(
+//         <div>
+//         <form onSubmit={ createBlock }>
+        
+//             <div>
+//                 <label>Color: </label> 
+//                 <input type="text"  />
+//                 <p> {} </p> 
+                
+//             </div>
+            
+            
+//             <input type="submit" value="Add" />
+//         </form>
+        
+        
+//         </div>
+//     );
+// };
+    
+// export default Form;
